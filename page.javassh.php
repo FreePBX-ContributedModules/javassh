955608186d1ca1139e0278f5bcf31a56890bf2cf
Rob Thomas <xrobau@gmail.com>
2006-04-29 22:41:48 +0000

    Create javassh module

---
+++ page.javassh.php
@@ -0,0 +1,30 @@
+<?php 
+/* $Id:$ */
+
+// Wrapper code for opening a new window. 
+//
+//This program is free software; you can redistribute it and/or
+//modify it under the terms of version 2 of the GNU General Public
+//License as published by the Free Software Foundation.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+?>
+
+</div> 
+<h2> Java SSH </h2>
+
+If necessary re-enter the maint user and password. Once the applet has opened enter the IP address of
+your PBX. Log in with the CentOS user name and password
+
+ <APPLET CODE="com.mindbright.application.MindTerm.class"
+          ARCHIVE="/admin/modules/javassh/mindterm.jar" WIDTH=0 HEIGHT=0>
+    <PARAM NAME="cabinets" VALUE="/admin/modules/javassh/mindterm.cab">
+    <PARAM NAME="sepframe" value="true">
+    <PARAM NAME="debug" value="false">
+</APPLET>
+
+
