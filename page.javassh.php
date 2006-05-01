c5a6d4b55d8a057a69676c64cefe9d1553e6cddf
Rob Thomas <xrobau@gmail.com>
2006-05-01 01:23:14 +0000

    Fix up formatting in javassh page

--- page.javassh.php
+++ page.javassh.php
@@ -14,17 +14,17 @@
 
 ?>
 
-</div> 
 <h2> Java SSH </h2>
 
 If necessary re-enter the maint user and password. Once the applet has opened enter the IP address of
 your PBX. Log in with the CentOS user name and password
 
- <APPLET CODE="com.mindbright.application.MindTerm.class"
+<APPLET CODE="com.mindbright.application.MindTerm.class"
           ARCHIVE="/admin/modules/javassh/mindterm.jar" WIDTH=0 HEIGHT=0>
     <PARAM NAME="cabinets" VALUE="/admin/modules/javassh/mindterm.cab">
     <PARAM NAME="sepframe" value="true">
     <PARAM NAME="debug" value="false">
 </APPLET>
 
+</div>
 
