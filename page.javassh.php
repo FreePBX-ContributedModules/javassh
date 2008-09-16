a07f7513b87235082e20de1303cff5dadd13028b
Micke Carlsson <mickecamino@gmail.com>
2008-09-16 09:05:41 +0000

    Enclosed text strings for localization, added swedish language to javassh

--- page.javassh.php
+++ page.javassh.php
@@ -12,13 +12,13 @@
 //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //GNU General Public License for more details.
 
+
+
+echo "<h2> "._("Java SSH")." </h2>";
+echo _("If necessary re-enter the maint user and password. Once the applet has opened enter the IP address of your PBX.<br>");
+echo _("You will then connect to the FreePBX server. You can then login as 'root' or 'asterisk', or whatever user you have created.");
+
 ?>
-
-<h2> Java SSH </h2>
-
-If necessary re-enter the maint user and password. Once the applet has opened enter the IP address of
-your PBX. You will then connect to the FreePBX server. You can then login as 'root' or 'asterisk', or
-whatever user you have created.
 
 <APPLET CODE="com.mindbright.application.MindTerm.class"
           ARCHIVE="/admin/modules/javassh/mindterm.jar" WIDTH=0 HEIGHT=0>
